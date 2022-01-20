f4dc9a7fb4690bec3639bb62c9715b0a694cab06
Codeonomics <rosaci8709@moxkid.com>
2022-01-20 20:57:56 +0000

    Initial Commit

---
+++ LocationPicker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LocationPicker
+//
+//  Created by Codeonomics on 20/01/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LocationPicker/LocationPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationPickerApp.swift
+//  LocationPicker
+//
+//  Created by Codeonomics on 20/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct LocationPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
